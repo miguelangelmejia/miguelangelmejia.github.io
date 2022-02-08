ebb81300f82c9ec76aeafb332075d8c911260397
Miguel Angel Mejia <m3mejia@ucsd.edu>
2022-02-07 16:08:06 -0800

    Add testimonial section to website.

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,15 @@
 		});
 
 })(jQuery);
+
+$(document).ready(function() {
+  $(".testimonial-carousel").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    prevArrow: $(".testimonial-carousel-controls .prev"),
+    nextArrow: $(".testimonial-carousel-controls .next")
+  });
+});
